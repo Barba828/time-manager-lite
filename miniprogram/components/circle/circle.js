1f773788511cf94eee95e54694d6347b4378f0f4
barba828 <1435315192@qq.com>
2020-06-23 00:19:55 +0800

    init

---
+++ miniprogram/components/circle/circle.js
@@ -0,0 +1,91 @@
+Component({
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  properties: {
+    name: {
+      type: String,
+    },
+    size: {
+      type: Number,
+      value: 100,
+    },
+    backColor: {
+      type: String,
+      value: '#ccc'
+    },
+    lineColor: {
+      type: String,
+      value: '#1cbbb4'
+    },
+    lineWidth: {
+      type: Number,
+      value: 10,
+    },
+  },
+
+  data: {
+    bg: 'bg',
+    draw: 'draw',
+  },
+  methods: {
+    setCircle: function () {
+      const {
+        size,
+        lineWidth
+      } = this.properties
+      let r = size / 2;
+      let ctx = wx.createCanvasContext(this.data.bg, this);
+      ctx.setLineWidth(lineWidth);
+      ctx.setStrokeStyle(this.properties.backColor);
+      ctx.setLineCap('round')
+      ctx.beginPath();
+      ctx.arc(r, r, r - lineWidth, 0, 2 * Math.PI, false); //(x,y,r)
+      ctx.stroke(); //对当前路径进行描边
+      ctx.draw();
+      return this;
+    },
+
+    drawCircle: function (percentage) {
+      const {
+        size,
+        lineWidth
+      } = this.properties;
+      let r = size / 2;
+      let step = percentage * 2;
+      // 使用 wx.createContext 获取绘图上下文 context  绘制彩色进度条圆环
+      let context = wx.createCanvasContext(this.properties.name, this);
+      context.setLineWidth(lineWidth);
+      context.setStrokeStyle(this.properties.lineColor);
+      context.setLineCap('round');
+      context.beginPath(); //开始一个新的路径
+      context.arc(r, r, r - lineWidth, -Math.PI / 2, step * Math.PI - Math.PI / 2, false);
+      context.stroke(); //对当前路径进行描边
+      context.draw();
+    },
+
+    run: function (percentage = 1, time = 1000, interval = 10) {
+      let unit = percentage / (time / interval);
+      let tempPercent = 0;this.countTimer = setInterval(() => {
+        if (tempPercent < percentage) {
+          // 绘制彩色圆环进度条
+          this.drawCircle(tempPercent)
+          tempPercent += unit
+        } else {
+          clearInterval(this.countTimer);
+        }
+      }, interval)
+    }
+  },
+
+  // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+  ready: function () {
+    console.log(this.properties.name);
+    this.setData({
+      // bg: this.properties.name+'-bg',
+      draw: 'draw',
+    }, () => {
+      this.setCircle();
+    });
+  }
+})
---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,147 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+
+    count: 0, //计数器，初始值为0
+    maxCount: 60, // 绘制一个圆环所需的步骤 
+  },
+
+  onLoad: function () {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  onGetUserInfo: function (e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  onGetOpenid: function () {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../userConsole/userConsole',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+
+  // 上传图片
+  doUpload: function () {
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+
+        wx.showLoading({
+          title: '上传中',
+        })
+
+        const filePath = res.tempFilePaths[0]
+
+        // 上传图片
+        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log('[上传文件] 成功：', res)
+
+            app.globalData.fileID = res.fileID
+            app.globalData.cloudPath = cloudPath
+            app.globalData.imagePath = filePath
+
+            wx.navigateTo({
+              url: '../storageConsole/storageConsole'
+            })
+          },
+          fail: e => {
+            console.error('[上传文件] 失败：', e)
+            wx.showToast({
+              icon: 'none',
+              title: '上传失败',
+            })
+          },
+          complete: () => {
+            wx.hideLoading()
+          }
+        })
+
+      },
+      fail: e => {
+        console.error(e)
+      }
+    })
+  },
+
+
+  countInterval: function () {
+    // 设置倒计时 定时器 假设每隔100毫秒 count递增+1，当 count递增到两倍maxCount的时候刚好是一个圆环（ step 从0到2为一周），然后改变txt值并且清除定时器
+    this.countTimer = setInterval(() => {
+      if (this.data.count <= 2 * this.data.maxCount) {
+        // 绘制彩色圆环进度条
+        this.circle1.drawCircle(this.data.count / this.data.maxCount)
+        this.data.count++;
+      } else {
+        this.setData({
+          txt: "匹配成功"
+        });
+        clearInterval(this.countTimer);
+      }
+    }, 10)
+  },
+
+  onReady: function () {
+    // 获得circle组件
+    this.circle = this.selectComponent("#circle");
+    this.circle.run(0.78)
+    // this.circle2 = this.selectComponent("#circle2");
+    // this.circle.run(0.3)
+  },
+})
